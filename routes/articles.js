4db1b3e9a19285a57ffb96f508373895eefa8760
Trav Colbert <trav.colbert@gmail.com>
2025-04-07 12:34:59 -0600

    more modular routing and view helpers

--- routes/articles.js
+++ routes/articles.js
@@ -3,19 +3,24 @@ const router = express.Router({ mergeParams: true })
 const path = require('path')
 
 module.exports = function (app) {
+    const viewHelper = require(path.join(__dirname, '../../', app.locals.appPath, app.locals.helperPath, 'view_helper'))
+
     const currentRouteName = path.basename(__filename, '.js')
 
     const controller = require(path.join(__dirname, '../../', app.locals.appPath, app.locals.controllerPath, currentRouteName))(app)
 
     router.route("/:id")
-        .get((req, res) => {
-            return controller.show(req, res)
-        })
+        .get([
+            controller.show,
+            viewHelper.parseArticle,
+            viewHelper.renderArticle
+        ])
 
     router.route("/")
-        .get((req, res) => {
-            controller.index(req, res)
-        })
+        .get([
+            controller.index,
+            viewHelper.renderArticles
+        ])
 
     return router
 }

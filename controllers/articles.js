272464f3780bc70bf34405f6512f325a31fb0379
Trav Colbert <trav.colbert@gmail.com>
2025-04-08 22:48:17 -0600

    removed unused methods\nadded article list for menus

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,22 +1,6 @@
 'use strict'
 const path = require('path')
 
-const sortByCreatedAt = function (a, b) {
-  const aCreatedAt = Date.parse(a.createdAt)
-  const bCreatedAt = Date.parse(b.createdAt)
-  return bCreatedAt - aCreatedAt
-}
-
-const sortByTitle = function (a, b) {
-  const aTitle = a.title.toLowerCase()
-  const bTitle = b.title.toLowerCase()
-  return aTitle.localeCompare(bTitle)
-}
-
-const filterByTag = function (article, tag) {
-  return article.tags && article.tags.includes(tag)
-}
-
 module.exports = function (app) {
   const currentControllerName = path.basename(__filename, '.js')
 
@@ -29,6 +13,7 @@ module.exports = function (app) {
     },
     show: function (req, res, next) {
       res.locals.article = app.locals.models[currentControllerName].find(req.params.id)
+      res.locals.articles = app.locals.models[currentControllerName].all()
       return next()
     }
   }

acce7286541721d7ab1ace52af2f8c85e9f4862a
Trav Colbert <trav.colbert@gmail.com>
2025-04-25 23:54:58 -0600

    accepts read token passed as environment var APP_TOKEN

--- jobs/job.sh
+++ jobs/job.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+echo https://app:$1@gitlab.com/TravColbert/traviscolbert.net_articles.git
 git clone https://app:$1@gitlab.com/TravColbert/traviscolbert.net_articles.git ./node-express-starter-app-blog/articles

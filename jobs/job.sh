db3ab8fbb30b5b51fe37e757e892904b7e3daf66
Trav Colbert <trav.colbert@gmail.com>
2025-04-26 00:19:11 -0600

    addressing access denied issues

--- jobs/job.sh
+++ jobs/job.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo https://app:$1@gitlab.com/TravColbert/traviscolbert.net_articles.git
-git clone https://app:$1@gitlab.com/TravColbert/traviscolbert.net_articles.git ./node-express-starter-app-blog/articles
+echo https://username:$1@gitlab.com/TravColbert/traviscolbert.net_articles.git
+git clone https://username:$1@gitlab.com/TravColbert/traviscolbert.net_articles.git ./node-express-starter-app-blog/articles

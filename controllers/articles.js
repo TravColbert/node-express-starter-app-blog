8122005950f38725addd002315dc50635dad2bd9
Trav Colbert <trav.colbert@gmail.com>
2025-05-02 14:29:17 -0600

    refactor: implement caching for article retrieval in index and show methods

--- controllers/articles.js
+++ controllers/articles.js
@@ -9,27 +9,37 @@ module.exports = function (app) {
   return {
     index: function (req, res, next) {
       if (req.query.tag) {
-        res.locals.render.articles = app.locals.models[currentControllerName].all(
-          function (article) {
-            return article.tags && article.tags.includes(req.query.tag)
-          }
-        )
+        res.locals.render.articles = app.locals.cache(`articles-tagged-${req.query.tag}`, () => {
+          return app.locals.models[currentControllerName].all(
+            function (article) {
+              return article.tags && article.tags.includes(req.query.tag)
+            }
+          )
+        })
         res.locals.render.title = `Articles tagged as: ${req.query.tag}' (${res.locals.render.articles.length})`
       } else {
-        res.locals.render.articles = app.locals.models[currentControllerName].all()
+        res.locals.render.articles = app.locals.cache(`articles-all`, function () {
+          return app.locals.models[currentControllerName].all()
+        })
         res.locals.render.title = `All Articles (${res.locals.render.articles.length})`
       }
       return next()
     },
     show: function (req, res, next) {
       if (req.params.id) {
-        res.locals.render.article = app.locals.models[currentControllerName].find(req.params.id)
+        res.locals.render.article = app.locals.cache(`article-${req.params.id}`, (req) => {
+          return app.locals.models[currentControllerName].find(req.params.id)
+        })
       } else {
         console.debug(`No article ID provided - getting latest article`)
-        res.locals.render.article = app.locals.models[currentControllerName].last()
+        res.locals.render.article = app.locals.cache(`article-latest`, (req) => {
+          return app.locals.models[currentControllerName].last()
+        })
       }
       res.locals.render.title = res.locals.render.article.metadata.title
-      res.locals.render.articles = app.locals.models[currentControllerName].all()
+      res.locals.render.articles = app.locals.cache(`articles-all`, () => {
+        return app.locals.models[currentControllerName].all()
+      })
       return next()
     }
   }

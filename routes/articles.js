2ca286a440ebf78bd0a0ed465c47d7da109897ec
Trav Colbert <trav.colbert@gmail.com>
2025-11-30 21:04:46 -0800

    refactor: simplify viewHelper and controller path resolution

--- routes/articles.js
+++ routes/articles.js
@@ -3,11 +3,11 @@ const router = express.Router({ mergeParams: true })
 const path = require('path')
 
 module.exports = function (app, appInstance) {
-    const viewHelper = require(path.join(__dirname, '..', app.locals.basePath, appInstance, app.locals.helperPath, 'view_helper'))(app)
+    const viewHelper = require(path.join(__dirname, '..', app.locals.helperPath, 'view_helper'))(app)
 
     const currentRouteName = path.basename(__filename, '.js')
 
-    const controller = require(path.join(__dirname, '..', app.locals.basePath, appInstance, app.locals.controllerPath, currentRouteName))(app)
+    const controller = require(path.join(__dirname, '..', app.locals.controllerPath, currentRouteName))(app)
 
     router.route("/:id")
         .get([

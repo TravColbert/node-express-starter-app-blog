431ad4b1e9ad27c0e24e234ed665a87d9d43656c
Trav Colbert <trav.colbert@gmail.com>
2025-12-03 17:54:58 -0800

    another adjustement to articles location

--- jobs/job.sh
+++ jobs/job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo https://username:${1}@gitlab.com/TravColbert/traviscolbert.net_articles.git
-git clone https://username:${1}@gitlab.com/TravColbert/traviscolbert.net_articles.git ./app/articles
+git clone https://username:${1}@gitlab.com/TravColbert/traviscolbert.net_articles.git ./articles

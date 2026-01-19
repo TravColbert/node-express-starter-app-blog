9add43f521d7401543ca0c70ea8eafb3c5dec678
Trav Colbert <trav.colbert@gmail.com>
2026-01-19 07:59:48 -0800

    Remove debug logs from static config

--- config/static.js
+++ config/static.js
@@ -4,9 +4,7 @@ const express = require("express");
 
 module.exports = function (app, appInstance) {
   const staticPath = path.join(__dirname, "..", "articles", "public");
-  console.debug(`***** ${staticPath}`);
   if (fs.existsSync(staticPath)) {
-    console.debug(`^^^ it's there!`);
     app.locals.debug &&
       console.debug(
         `Loading static config for: ${appInstance} at ${staticPath}`,
